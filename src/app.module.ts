a30badcf59133bc01f3a932c02ac4ba27e93815e
mantaGIT <hbkim0014@gmail.com>
2024-10-07 09:43:25 +0900

    chore: arrange AppModule
    - 불필요한 App 모듈 구성 삭제
    
    - TypeORM을 이용한 DB 연결
    
    - 환경변수 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { RegularModule } from './regular/regular.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV === 'development' ? '.env' : undefined,
+      isGlobal: true, // 전체 모듈에서 사용할 수 있도록 설정
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWD,
+      database: 'testdb',
+      synchronize: true, // 저장된 데이터가 사라질 수 있으니 개발 중에만 사용
+      autoLoadEntities: true,
+      // logging: true,
+      // logger: 'advanced-console',
+    }),
+    RegularModule,
+  ],
 })
 export class AppModule {}
