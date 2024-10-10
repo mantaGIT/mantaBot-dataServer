a0c3e2e717e6510ee0076b927939f9ae2fb6f561
mantaGIT <hbkim0014@gmail.com>
2024-10-10 15:18:01 +0900

    test: add unit tests for regular-service
    - automock 적용
    - regular controller 테스트 추가
    - regular service의 parse 함수 테스트 추가
    - 파싱 테스트를 위한 원본 데이터 일부 파일로 저장

--- src/regular/regular.controller.spec.ts
+++ src/regular/regular.controller.spec.ts
@@ -1,20 +1,37 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { TestBed } from '@automock/jest';
 import { RegularController } from './regular.controller';
 import { RegularService } from './regular.service';
+import { Regular } from './entities/regular.entity';
 
 describe('RegularController', () => {
   let controller: RegularController;
+  let service: jest.Mocked<RegularService>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [RegularController],
-      providers: [RegularService],
-    }).compile();
-
-    controller = module.get<RegularController>(RegularController);
+    const { unit, unitRef } = TestBed.create(RegularController).compile();
+    controller = unit;
+    service = unitRef.get(RegularService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('서비스의 findAll() 함수를 호출해야 한다.', async () => {
+      service.findAll.mockResolvedValue([new Regular()]);
+      const result = await controller.findAll();
+      expect(service.findAll).toHaveBeenCalled();
+      expect(result).toBeInstanceOf(Array<Regular>);
+    });
+  });
+
+  describe('findOne', () => {
+    it('서비스의 findOne() 함수를 호출해야 한다.', async () => {
+      service.findOne.mockResolvedValue(new Regular());
+      const result = await controller.findOne(1);
+      expect(service.findOne).toHaveBeenCalled();
+      expect(result).toBeInstanceOf(Regular);
+    });
   });
 });
--- src/regular/regular.service.spec.ts
+++ src/regular/regular.service.spec.ts
@@ -1,37 +1,19 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { RegularService } from './regular.service';
 import { Regular } from './entities/regular.entity';
-import { getRepositoryToken } from '@nestjs/typeorm';
 import { ConflictException, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
-
-type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
-const mockRepository = () => ({
-  create: jest.fn(),
-  save: jest.fn(),
-  find: jest.fn(),
-  findOneBy: jest.fn(),
-  remove: jest.fn(),
-});
+import { TestBed } from '@automock/jest';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { schedData, localeData } from './test-data/test-api-data';
 
 describe('RegularService', () => {
   let service: RegularService;
-  let repository: MockRepository<Regular>;
+  let repository: jest.Mocked<Repository<Regular>>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        RegularService,
-        { provide: getRepositoryToken(Regular), useValue: mockRepository() },
-      ],
-    }).compile();
-
-    service = module.get<RegularService>(RegularService);
-    repository = module.get(getRepositoryToken(Regular));
-  });
-
-  afterEach(async () => {
-    jest.clearAllMocks();
+    const { unit, unitRef } = TestBed.create(RegularService).compile();
+    service = unit;
+    repository = unitRef.get(getRepositoryToken(Regular) as string);
   });
 
   it('should be defined', () => {
@@ -49,13 +31,13 @@ describe('RegularService', () => {
     const mockEntity = mockDTO;
 
     it('입력받은 데이터를 저장하고 반환한다.', async () => {
-      jest.spyOn(repository, 'save').mockReturnValue(mockEntity);
+      repository.save.mockResolvedValue(mockEntity);
       const result = await service.create(mockDTO);
       expect(result).toEqual(mockEntity);
     });
 
     it('해당 ID 데이터가 이미 존재한다면 ConflictException 에러를 발생시킨다.', async () => {
-      jest.spyOn(repository, 'findOneBy').mockReturnValue(mockEntity);
+      repository.findOneBy.mockResolvedValue(mockEntity);
       try {
         await service.create(mockDTO);
       } catch (error) {
@@ -68,7 +50,7 @@ describe('RegularService', () => {
     const mockDB = [];
 
     it('전체 데이터를 배열로 반환한다.', async () => {
-      jest.spyOn(repository, 'find').mockReturnValue(mockDB);
+      repository.find.mockResolvedValue(mockDB);
       const result = await service.findAll();
       expect(result).toBeInstanceOf(Array);
       expect(result).toEqual(mockDB);
@@ -86,22 +68,14 @@ describe('RegularService', () => {
 
     it('해당 ID 데이터가 존재할 경우 이를 반환한다.', async () => {
       const validID = mockEntity.id;
-      jest
-        .spyOn(repository, 'findOneBy')
-        .mockImplementation(({ id }) =>
-          id === mockEntity.id ? mockEntity : null,
-        );
+      repository.findOneBy.mockResolvedValue(mockEntity);
       const result = await service.findOne(validID);
       expect(result).toEqual(mockEntity);
     });
 
     it('해당 ID 데이터가 존재하지 않을 경우 NotFoundException 에러를 발생시킨다.', async () => {
       const invalidID = mockEntity.id + 1;
-      jest
-        .spyOn(repository, 'findOneBy')
-        .mockImplementation(({ id }) =>
-          id === mockEntity.id ? mockEntity : null,
-        );
+      repository.findOneBy.mockResolvedValue(null);
       try {
         await service.findOne(invalidID);
       } catch (error) {
@@ -130,18 +104,35 @@ describe('RegularService', () => {
 
     it('모든 데이터를 삭제하고 삭제된 데이터를 반환한다.', async () => {
       const rows = mockDB.length;
-      jest.spyOn(repository, 'find').mockReturnValue(mockDB);
-      jest.spyOn(repository, 'remove').mockImplementation((mockDB) => {
-        const copyDB: Array<Regular> = [...mockDB];
+      repository.find.mockResolvedValue(mockDB);
+      repository.remove = jest.fn().mockImplementation((db) => {
+        expect(db).toEqual(mockDB);
+        const copyDB: Array<any> = [...db];
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const removed = copyDB.map(({ id, ...rest }) => rest);
-        mockDB.length = 0;
-        return removed;
+        db.length = 0;
+        return Promise.resolve(removed);
       });
       const result = await service.removeAll();
       expect(mockDB.length).toEqual(0);
       expect(result.length).toEqual(rows);
-      expect(result).toBeInstanceOf(Array<Partial<Regular>>);
+      expect(result).toBeInstanceOf(Array);
+      expect(result[0]).not.toHaveProperty('id');
+    });
+  });
+
+  describe('parse', () => {
+    it('파싱한 데이터가 예상 데이터 값과 동일한 값을 갖는지 확인한다.', async () => {
+      const parsedData = {
+        id: 0,
+        startTime: '2024-10-09T16:00:00Z',
+        endTime: '2024-10-09T18:00:00Z',
+        rule: '영역 배틀',
+        stages: ['넙치 언덕 단지', '해녀 미술 대학'],
+      };
+
+      const result = await service.parse(schedData, localeData);
+      expect(result).toEqual([parsedData]);
     });
   });
 });
