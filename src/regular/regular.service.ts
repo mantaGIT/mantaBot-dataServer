4c39cec726b98ff0c5947e2e74309f3a2b4c9ae7
mantaGIT <hbkim0014@gmail.com>
2024-10-17 19:39:02 +0900

    fix: error logger
    - 로그에 에러 스택 출력 제외
    - fetch-scheduler 서비스에 에러 로그 적용
    - api-fetch 서비스에 에러 로그 및 throw 적용
    - regular 서비스에 에러 로그 및 thorw 적용

--- src/regular/regular.service.ts
+++ src/regular/regular.service.ts
@@ -17,6 +17,11 @@ export class RegularService {
     private readonly logger: CustomLoggerService,
   ) {}
 
+  /**
+   * Regualr 테이블 데이터를 여러 개 생성합니다.
+   * @param createRegularDtos DTO 배열
+   * @returns
+   */
   async saveAll(createRegularDtos: CreateRegularDto[]): Promise<Regular[]> {
     let result: Regular[] = [];
     try {
@@ -31,32 +36,32 @@ export class RegularService {
       );
       this.logger.log('Save all schedules', this.CONTEXT);
     } catch (error) {
-      this.logger.error('Save error', error.stack, this.CONTEXT);
-      // throw new InternalServerErrorException(error);
+      const errorMsg = ['Save error', error.message].join(' - ');
+      this.logger.error(errorMsg, error.stack, this.CONTEXT);
+      throw error;
     }
     return result;
   }
 
+  /**
+   * Regular 테이블 전체 조회
+   * @returns
+   */
   async findAll(): Promise<Regular[]> {
     try {
       this.logger.log('Find schedules', this.CONTEXT);
       return this.regularRepository.find();
     } catch (error) {
-      this.logger.error('Find error', error.stack, this.CONTEXT);
-      // throw new InternalServerErrorException(error);
+      const errorMsg = ['Find error', error.message].join(' - ');
+      this.logger.error(errorMsg, error.stack, this.CONTEXT);
+      throw error;
     }
   }
 
-  // async findOne(id: number): Promise<Regular> {
-  //   // findOneBy() : 찾지 못할 경우 return null
-  //   const regular = await this.regularRepository.findOneBy({ id: id });
-  //   if (!regular)
-  //     throw new NotFoundException(
-  //       `ID #${id}에 해당하는 데이터를 찾을 수 없습니다.`,
-  //     );
-  //   return regular;
-  // }
-
+  /**
+   * Regular 테이블 데이터를 전체 삭제합니다.
+   * @returns
+   */
   async removeAll(): Promise<Regular[]> {
     let result: Regular[] = [];
     try {
@@ -69,34 +74,48 @@ export class RegularService {
       this.logger.log('Remove all schedules', this.CONTEXT);
       return result;
     } catch (error) {
-      this.logger.error('Remove error', error.stack, this.CONTEXT);
-      // throw new InternalServerErrorException(error);
+      const errorMsg = ['Remove error', error.message].join(' - ');
+      this.logger.error(errorMsg, error.stack, this.CONTEXT);
+      throw error;
     }
   }
 
+  /**
+   * 원본 데이터를 테이블 Entity에 맞게 파싱합니다.
+   * @param schedData 원본 스케줄 데이터
+   * @param localeData 원본 한글화 데이터
+   * @returns
+   */
   async parse(
     schedData: object,
     localeData: object,
   ): Promise<CreateRegularDto[]> {
-    const nodes: Array<object> = lodashGet(
-      schedData,
-      `data.regularSchedules.nodes`,
-    );
+    try {
+      const nodes: Array<object> = lodashGet(
+        schedData,
+        `data.regularSchedules.nodes`,
+      );
 
-    const regulars: CreateRegularDto[] = nodes.map((regularNode, index) => {
-      const setting = lodashGet(regularNode, 'regularMatchSetting');
-      const ruleId = lodashGet(setting, 'vsRule.id');
-      const stageIds = lodashMap(lodashGet(setting, 'vsStages'), 'id');
-      return {
-        id: index,
-        startTime: regularNode['startTime'],
-        endTime: regularNode['endTime'],
-        rule: localeData['rules'][ruleId]['name'],
-        stages: stageIds.map(
-          (stageId) => localeData['stages'][stageId]['name'],
-        ),
-      };
-    });
-    return regulars;
+      const regulars: CreateRegularDto[] = nodes.map((regularNode, index) => {
+        const setting = lodashGet(regularNode, 'regularMatchSetting');
+        const ruleId = lodashGet(setting, 'vsRule.id');
+        const stageIds = lodashMap(lodashGet(setting, 'vsStages'), 'id');
+        return {
+          id: index,
+          startTime: regularNode['startTime'],
+          endTime: regularNode['endTime'],
+          rule: localeData['rules'][ruleId]['name'],
+          stages: stageIds.map(
+            (stageId) => localeData['stages'][stageId]['name'],
+          ),
+        };
+      });
+      this.logger.log(`Parse success`, this.CONTEXT);
+      return regulars;
+    } catch (error) {
+      const errorMsg = [`Parse error`, error.message].join(' - ');
+      this.logger.error(errorMsg, error.stack, this.CONTEXT);
+      throw error;
+    }
   }
 }
