368216ce22972af9990f8acd3df5ecac395a9f9b
mantaGIT <hbkim0014@gmail.com>
2024-10-17 11:10:03 +0900

    feat: apply logs
    - fetch-scheduler 콘솔 출력 정리
    - api-fetch 로그 추가

--- src/api-fetch/api-fetch.service.ts
+++ src/api-fetch/api-fetch.service.ts
@@ -1,21 +1,30 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import { AxiosError, AxiosResponse } from 'axios';
+import { Injectable } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
+import { CustomLoggerService } from 'src/custom-logger/custom-logger.service';
 
 @Injectable()
 export class ApiFetchService {
-  constructor(private readonly httpService: HttpService) {}
+  private readonly CONTEXT = 'ApiFetch';
+
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly logger: CustomLoggerService,
+  ) {}
 
   async apiFetch(apiUrl: string): Promise<AxiosResponse<object>> {
     try {
       const response = await this.httpService.axiosRef.get(apiUrl);
+      this.logger.verbose(`Fetch : ${apiUrl}`, this.CONTEXT); // log
       return response.data;
     } catch (error) {
-      const errorMsg =
-        error instanceof AxiosError
-          ? `API Fetch Error - ${apiUrl}, (${error.response.status}) ${error.response.statusText}`
-          : error.message;
-      throw new InternalServerErrorException(errorMsg);
+      // console.log(error);
+      // const errorMsg =
+      //   error instanceof AxiosError
+      //     ? `API Fetch Error - ${apiUrl}, (${error.response.status}) ${error.response.statusText}`
+      //     : error.message;
+      this.logger.error(error.message, error.stack, this.CONTEXT); // log
+      // throw new InternalServerErrorException(error.message);
     }
   }
 }
--- src/fetch-scheduler/fetch-scheduler.service.ts
+++ src/fetch-scheduler/fetch-scheduler.service.ts
@@ -1,36 +1,30 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { ApiFetchService } from 'src/api-fetch/api-fetch.service';
+import { CustomLoggerService } from 'src/custom-logger/custom-logger.service';
 import { RegularService } from 'src/regular/regular.service';
 
 @Injectable()
 export class FetchSchedulerService implements OnModuleInit {
+  private readonly CONTEXT = 'FetchScheduler';
+
   constructor(
+    private readonly logger: CustomLoggerService,
     private readonly apiFetchService: ApiFetchService,
     private readonly regularService: RegularService,
   ) {}
 
   @Cron('30 0 */2 * * *', { timeZone: 'UTC' })
   async fetchSchedule() {
+    const sched = await this.apiFetchService.apiFetch(process.env.SCHED_URL);
+    const locale = await this.apiFetchService.apiFetch(process.env.LOCALE_URL);
+
     // 기존 데이터 전체 삭제하기
     await this.regularService.removeAll();
-    console.log('remove all regular schedules');
-
-    // 원본 데이터 가져오기
-    const schedules = await this.apiFetchService.apiFetch(
-      process.env.SCHED_URL,
-    );
-    console.log('fetch schedules:', process.env.SCHED_URL);
-    const locale = await this.apiFetchService.apiFetch(process.env.LOCALE_URL);
-    console.log('fetch locale:', process.env.LOCALE_URL);
 
     // 데이터 파싱하여 저장하기
-    (await this.regularService.parse(schedules, locale)).forEach(
-      (regularDTO) => {
-        this.regularService.create(regularDTO);
-        console.log('save a new regular schedule');
-      },
-    );
+    const parsedRegulars = await this.regularService.parse(sched, locale);
+    await this.regularService.saveAll(parsedRegulars);
   }
 
   onModuleInit() {
